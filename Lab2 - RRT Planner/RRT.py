695ad986831487c601b5441fca6ca966ad3512e2
QuantuMope <43973701+QuantuMope@users.noreply.github.com>
2019-10-30 19:16:10 -0700

    rrt* attempt

--- Lab2 - RRT Planner/RRT.py
+++ Lab2 - RRT Planner/RRT.py
@@ -21,20 +21,24 @@ class RRT_Node:
         self.y = initial_state[1]
         self.theta = initial_state[2]
         self.parent = None
+        self.cost = 0
         self.actions = []
         self.path = []
 
-    def get_xy(self):
-        return np.array([self.x, self.y])
-
     def get_state(self):
-        return self.x, self.y, self.theta
+        return np.array([self.x, self.y, self.theta])
 
-    def set_parent_path(self, parent_node, actions, path):
+    # def get_state(self):
+    #     return self.x, self.y, self.theta
+
+    def set_parent(self, parent_node, actions, path):
         self.parent = parent_node
         self.actions = actions
         self.path = path
+        self.cost = self.parent.cost + len(self.actions)
 
+    def equals(self, other_node):
+        return self.get_state() == other_node.get_state()
 
 class RRT_Robot:
     def __init__(self, parking_plot):
@@ -108,11 +112,11 @@ class RRT_Robot:
     # Question 2(a)
     def _find_nearest_node(self, sample_point):
         root = self.node_list[0]
-        shortest_dist = np.linalg.norm(root.get_xy() - sample_point)
+        shortest_dist = np.linalg.norm(root.get_state() - sample_point)
         min_index = 0
         nearest_node = root
         for i, node in enumerate(self.node_list):
-            dist = np.linalg.norm(node.get_xy() - sample_point)
+            dist = np.linalg.norm(node.get_state() - sample_point)
             if dist < shortest_dist:
                 shortest_dist = dist
                 min_index = i
@@ -214,17 +218,17 @@ class RRT_Robot:
         goal_polygon = Polygon([(left, bottom), (left, top), (right, top), (right, bottom)])
         while True:
             sample_point = utils.sample_random_point(0, 2000, 0, 1400, 0, 360)
-            if collision_counter % 50 == 0:
+            if node_counter % 50 == 0:
                 sample_point = utils.sample_random_point(left, right, bottom, top+200, 170, 190)
+                node_counter += 1
             if self._check_collision(sample_point): continue
-            closest_point, closest_index = self._find_nearest_node(np.array([sample_point[0], sample_point[1]]))
-            curr_node = self.node_list[closest_index]
-            control_inputs, trajectory, new_node = self._generate_trajectory(curr_node.get_state(), sample_point)
+            closest_node, closest_index = self._find_nearest_node(np.array([sample_point[0], sample_point[1], sample_point[2]]))
+            control_inputs, trajectory, new_node = self._generate_trajectory(closest_node.get_state(), sample_point)
             if control_inputs is None:
                 print("Collision Count: {}".format(collision_counter))
                 collision_counter += 1
                 continue
-            new_node.set_parent_path(curr_node, control_inputs, trajectory)
+            new_node.set_parent(closest_node, control_inputs, trajectory)
             self.node_list.append(new_node)
             if goal_polygon.contains(Point(new_node.x, new_node.y)):
                 print("Path Found!")
@@ -235,14 +239,14 @@ class RRT_Robot:
             node_counter += 1
             print("Node Count: {}".format(node_counter))
 
-        # Plot the tree.
+    def plot_rrt_tree(self):
         for node in self.node_list:
             prev_node = node.parent
             if prev_node is None: continue
             self.tree_ax.plot([prev_node.x, node.x], [prev_node.y, node.y], 'magenta')
             self.tree_ax.plot(node.x, node.y, 'b.')
 
-    def plot_path(self):
+    def plot_path(self, animation=False):
         curr_node = self.node_list[-1]
         trajectory = []
         while curr_node is not None:
@@ -257,9 +261,107 @@ class RRT_Robot:
             t = tr + ts
             frame.set_transform(t)
             self.trajectory_ax.add_patch(frame)
-            self.trajectory_ax.plot(x, y, 'bo')
+            self.trajectory_ax.plot(x, y, 'bo', markersize=2)
+            if animation:
+                plt.pause(0.01)
 
-    # def RRT_star_plan(self):
+    def _find_better_node(self, x_new, orig_parent, threshold=75):
+        better_node = orig_parent
+        for node in self.node_list:
+            if np.abs(np.linalg.norm(node.get_state() - x_new.get_state())) < threshold and node.cost < better_node.cost:
+                better_node = node
+        return better_node
+
+    def rewire(self, threshold=350):
+        all_states = []
+        for node in self.node_list:
+            all_states.append(node.get_state())
+        all_states = np.array(all_states)
+
+        rewire_check = 0
+        rewire_counter = 0
+        for node in self.node_list:
+            rewire_check += 1
+            if node.parent is None: continue
+            orig_parent = node.parent
+            orig_cost = node.cost
+            better_node = node.parent
+            if rewire_check % 50 == 0: print("Rewire Check for Node: {}".format(rewire_check))
+            print("gets_caught_earlier")
+            candidates = np.argwhere(np.sqrt(np.sum((all_states - node.get_state())**2, axis=1)) < threshold)
+            for i in candidates:
+                print("gets_caught")
+                if self.node_list[i[0]].cost < better_node.cost:
+                    better_node = self.node_list[i[0]]
+            # if better_node.equals(orig_parent): continue
+            rewire_control = []
+            rewire_trajectory = []
+            rewire_cost = better_node.cost
+            new_node = better_node
+            while True:
+                print("enters loop")
+                control_inputs, trajectory, new_node = self._generate_trajectory(new_node.get_state(), node.get_state())
+                if rewire_cost > orig_cost or control_inputs is None: break
+                rewire_cost += len(control_inputs)
+                rewire_control = rewire_control + control_inputs
+                rewire_trajectory = rewire_trajectory + trajectory
+                print("This is broken")
+                if np.all(np.abs(np.array([new_node.x-node.x, new_node.y-node.y, new_node.theta-node.theta])) < 3):
+                    node.set_parent(better_node, rewire_control, rewire_trajectory)
+                    node.cost = orig_cost
+                    rewire_counter += 1
+                    print("Rewired Count: {}".format(rewire_counter))
+                    break
+
+    def RRT_star_plan(self, initial_state, goal_state, max_iters=10):
+        """
+        Optimized version of RRT planning algorithm with key differences.
+        1. New nodes are connected to closest node after trajectory not before.
+        2. After a new node is created, all nodes are reconnected for shortest path.
+        """
+        self.node_list.append(RRT_Node(initial_state))
+        node_counter = 0
+        collision_counter = 0
+        better_node_counter = 0
+        bottom, top, left, right = utils.obstacle_to_corner(goal_state)
+        goal_polygon = Polygon([(left, bottom), (left, top), (right, top), (right, bottom)])
+        while True:
+            sample_point = utils.sample_random_point(0, 2000, 0, 1400, 0, 360)
+            if node_counter % 50 == 0:
+                sample_point = utils.sample_random_point(left, right, bottom, top+200, 170, 190)
+                node_counter += 1
+            if self._check_collision(sample_point): continue
+            closest_node, closest_index = self._find_nearest_node(np.array([sample_point[0], sample_point[1], sample_point[2]]))
+            control_inputs, trajectory, new_node = self._generate_trajectory(closest_node.get_state(), sample_point)
+            if control_inputs is None:
+                collision_counter += 1
+                # print("Collision Count: {}".format(collision_counter))
+                continue
+
+            # Check for better possible node.
+            better_node = self._find_better_node(new_node, closest_node)
+            control_inputs, trajectory, new_node = self._generate_trajectory(better_node.get_state(), new_node.get_state())
+            if control_inputs is None:
+                collision_counter += 1
+                print("Collision Count: {}".format(collision_counter))
+                continue
+
+            better_node = new_node
+            node_counter += 1
+            print("Node Count: {}".format(node_counter))
+
+            new_node.set_parent(better_node, control_inputs, trajectory)
+            self.node_list.append(new_node)
+            if goal_polygon.contains(Point(new_node.x, new_node.y)):
+                print("Path Found!")
+                break
+            if node_counter + better_node_counter > max_iters:
+                print("Path could not be found after {} samples.".format(max_iters))
+                break
+
+        self.rewire()
+
+
 
 def main():
     env = ParkingLot()
@@ -269,6 +371,8 @@ def main():
     robot.compute_config_space(env.obstacles)
     # robot.plot_config_space()
     robot.RRT_plan(initial_state, env.goals)
+    # robot.RRT_star_plan(initial_state, env.goals)
+    robot.plot_rrt_tree()
     robot.plot_path()
     plt.show()
 
